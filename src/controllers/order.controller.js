c93826577f614880aae17b223af24c334e3f3a18
umeshmehta14 <ishaanmehta782@gmail.com>
2024-02-05 16:12:35 +0530

    fix: orderList

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -8,7 +8,7 @@ const addOrder = asyncHandler(async (req, res) => {
   const { paymentId, products, amount, address } = req.body;
   const userId = req.user?._id;
 
-  if (orderList?.length === 0) {
+  if (products?.length === 0) {
     throw new ApiError(400, "Please add items to the order before placing it.");
   }
 
